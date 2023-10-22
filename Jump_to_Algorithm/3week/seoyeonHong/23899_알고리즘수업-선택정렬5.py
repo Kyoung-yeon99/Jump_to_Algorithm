7f8ddba5024df53c1fd61137dcc31a0a3efd82d0
seoyeon22 <hsy2709@gmail.com>
2023-10-23 00:08:15 +0900

    시간초과

--- Jump_to_Algorithm/3week/seoyeonHong/23899_알고리즘수업-선택정렬5.py
+++ Jump_to_Algorithm/3week/seoyeonHong/23899_알고리즘수업-선택정렬5.py
@@ -1,36 +1,30 @@
+# 시간 초과
 import sys
 
 n = int(sys.stdin.readline())
 arrA = list(map(int, sys.stdin.readline().split()))
 arrB = list(map(int, sys.stdin.readline().split()))
-flag = False
-countA = 0
-countB = 0
 
-if arrA == arrB or sorted(arrA) == arrB:
-    print(1)
-else:
-    for i in range(n-1, 0, -1):
-        if arrB[i] < arrB[i-1]:
+for last in range(n-1, 1, -1):
+    flag = True
+    max_idx = 0
+    for i in range(1, last+1):
+        if arrA[i] > arrA[max_idx]:
+            max_idx = i
+
+    if last != max_idx:
+        temp = arrA[max_idx]
+        arrA[max_idx] = arrA[last]
+        arrA[last] = temp
+
+    for i in range(n):
+        if arrA[i] != arrB[i]:
+            flag = False
             break
-        else:
-            countB += 1
 
-    for last in range(n-1, 0, -1):
-        max_idx = 0
-        for i in range(1, last+1):
-            if arrA[i] > arrA[max_idx]:
-                max_idx = i
-        if last != max_idx:
-            temp = arrA[max_idx]
-            arrA[max_idx] = arrA[last]
-            arrA[last] = temp
-        countA += 1
-        if countA == countB:
-            if arrA[:countA] == arrB[:countA]:
-                print(1)
-                flag = True
-            break
-    if flag is False:
-        print(0)
-
+    if flag is True:
+        print(1)
+        break
+    
+if flag is False:
+    print(0)
