128aaf5b514256a1dc41be5437f8faf43beeb978
seongyeong kim <101620455+kseon0828@users.noreply.github.com>
2023-11-29 17:39:17 +0900

    ADD) 10815 - 숫자카드

---
+++ Jump_to_Algorithm/9week/seongyeongKim/1129_BOJ_10815.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	long long N, M;
+	long long input;
+
+	vector<long long> v;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+	sort(v.begin(), v.end());
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		cin >> input;
+		cout << binary_search(v.begin(), v.end(), input) << ' ';
+	}
+
+	return 0;
+}
