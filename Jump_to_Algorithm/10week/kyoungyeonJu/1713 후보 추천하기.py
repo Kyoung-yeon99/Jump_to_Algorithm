f17aa8100d0fbaad9bb5b70543c5ac1de2b36416
Kyoung-yeon99 <itme2019@duksung.ac.kr>
2023-12-12 01:38:08 +0900

    week10

--- Jump_to_Algorithm/10week/kyoungyeonJu/1713 후보 추천하기.py
+++ Jump_to_Algorithm/10week/kyoungyeonJu/1713 후보 추천하기.py
@@ -1,8 +1,31 @@
-n = int(input())
-pics = [0]*n
-m = int(input())
-nums = list(map(int, input().split()))
+n = int(input())  # 사진틀 개수
+m = int(input())  # 총 추천 횟수
+nums = list(map(int, input().split()))  # 추천 번호
+pics = dict()
+pics_key = []
 
 for num in nums:
-    
+    if num in pics: # 이미 게시된 학생 사진
+        pics[num] += 1
+    else:  # 아직 게시되지 않은 학생
+        if len(pics) >= n:  # 남은 사진 틀이 없다면
+            # 추천 횟수가 가장 적은 학생번호 찾기
+            remove = []
+            for k, v in pics.items():
+                if v == min(pics.values()):
+                    remove.append(k)
+            # pics_key에서 먼저 나오는 거 찾기
+            for key in pics_key:  # pics_key는 리스트이기 때문에 순서가 있다
+                if key in remove:
+                    pics.pop(key)
+                    pics_key.remove(key)
+                    break
+        pics[num] = 1
+        pics_key.append(num)
+
+pics_key.sort()
+print(*pics_key)
+
+
+
 
--- Jump_to_Algorithm/10week/kyoungyeonJu/4396 지뢰 찾기.py
+++ Jump_to_Algorithm/10week/kyoungyeonJu/4396 지뢰 찾기.py
@@ -5,41 +5,30 @@ def bomb_cnt(a, b):
         ny = b + dy[i]
         if 0 <= nx < n and 0 <= ny < n and bomb[nx][ny] == '*':
             cnt += 1
-
     return cnt
 
 
 n = int(input())
-bomb = []  # 지뢰 위치
-opens = []  # 열린 칸
+bomb = [list(input()) for _ in range(n)]  # 지뢰 위치
+opens = [list(input()) for _ in range(n)]  # 열린 칸
 result = [['.'] * n for _ in range(n)]
 dx = [- 1, 0, 1, 1, 1, 0, - 1, - 1]
 dy = [- 1, - 1, - 1, 0, 1, 1, 1, 0]
-
-for _ in range(n):
-    row = list(input())
-    bomb.append(row)
-
-for _ in range(n):
-    row = list(input())
-    opens.append(row)
-
 is_bomb = False
+
 for i in range(n):
     for j in range(n):
-        if opens[i][j] == 'x' and bomb[i][j] == '*':  # 지뢰를 열었으면
-            is_bomb = True
-            break
-        elif opens[i][j] == 'x':  # 그냥 열었으면
+        if opens[i][j] == 'x':
+            if bomb[i][j] == '*':  # 지뢰를 열었으면
+                is_bomb = True
+                continue
             result[i][j] = bomb_cnt(i, j)
-
-    if is_bomb:
-        break
-
 
 if is_bomb:
     for i in range(n):
-        print('*'*n)
-else:
-    for i in range(n):
-        print(''.join(list(map(str, result[i]))))
+        for j in range(n):
+            if bomb[i][j] == '*':
+                result[i][j] = '*'
+
+for i in range(n):
+    print(''.join(list(map(str, result[i]))))
