388b341a7bc439ec03d5e2ce4314fabeef7531e2
allzeroyou <ekdudd119@naver.com>
2024-02-19 22:34:37 +0900

    week4) PS 6/6

--- Jump_to_Algorithm2/week1/Seoyeon Hong/1439_뒤집기.py
+++ Jump_to_Algorithm2/week1/Seoyeon Hong/1439_뒤집기.py
@@ -1,7 +1,7 @@
-os = input() # 처음 문자열
-ns = os[0] # 새로운 문자열(시작 숫자는 같음)
+os = input()  # 처음 문자열
+ns = os[0]  # 새로운 문자열(시작 숫자는 같음)
 for s in os:
-    if s != ns[-1]: # 숫자가 바뀔 경우
-        ns += s # 해당 숫자 저장
-count = ns.count('0') # 새로운 문자열에 있는 0의 개수
-print(min(count, len(ns) - count)) # 0 또는 1의 개수만큼 뒤집어야함을 이용
+    if s != ns[-1]:  # 숫자가 바뀔 경우
+        ns += s  # 해당 숫자 저장
+count = ns.count('0')  # 새로운 문자열에 있는 0의 개수
+print(min(count, len(ns) - count))  # 0 또는 1의 개수만큼 뒤집어야함을 이용
--- Jump_to_Algorithm2/week2/dayeongYoo/1789 수들의 합.py
+++ Jump_to_Algorithm2/week2/dayeongYoo/1789 수들의 합.py
@@ -1,17 +1,19 @@
+# 서로 다른 n개의 자연수 합: s
+# 자연수 n의 최댓값?
+
 s = int(input())
-# 개수 세기
+# 합 변수
 total = 0
 cnt = 0
 
 for i in range(1, s + 1):
-    cnt += 1
     total += i
-
-    if total > s:  # total==s이 아닌 이유는 s를 초과하는 경우를 감지하고 그때까지의 개수를 출력하기 위함임.
+    cnt += 1
+    if total > s:
         break
 
-# 예외 처리
-if s == 1:
+
+if s==1:
     print(1)
 else:
-    print(cnt - 1)  # s 초과하는 경우이므로, -1 해줌.
+    print(cnt - 1)  # total이 s보다 큰 경우이므로, -1 해준다
