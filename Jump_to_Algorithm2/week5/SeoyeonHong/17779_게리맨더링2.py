db8cdd8834a2efd15368ff2b0626a374933d0c5a
Seoyeon Hong <70433999+seoyeon22@users.noreply.github.com>
2024-02-29 01:14:52 +0900

    week5

--- Jump_to_Algorithm2/week5/SeoyeonHong/17779_게리맨더링2.py
+++ Jump_to_Algorithm2/week5/SeoyeonHong/17779_게리맨더링2.py
@@ -1,3 +1,4 @@
+# 오답...
 # 선거구를 나누는 방법 중에서, 인구가 가장 많은 선거구와 가장 적은 선거구의 인구 차이의 최솟값
 import sys
 import heapq
@@ -15,35 +16,39 @@ for _ in range(N):
     A.append(row)
 
 def calculate(x, y, d1, d2):
+    s = [[5 for _ in range(N)] for _ in range(N)]
     p = [0 for _ in range(5)] # 선거구별 인구
-    for r in range(1, x+d1): # 1번 선거구
-        for c in range(1, y+1):
-            p[0] += A[r-1][c-1]
-            if r+c == x+y-1: # 5번 선거구 경계선을 만나면 다음 행으로 넘어가기
-                break
 
-    for r in range(1, x+d2+1): # 2번 선거구
-        for c in range(y, N+1):
-            p[1] += A[r-1][c-1]
-            if r+c == x-y-1:
-                break
-
-    for r in range(x+d1, N+1): # 3번 선거구
-        for c in range(1, y-d1+d2):
-            p[2] += A[r-1][c-1]
-            if r+c == x+y-d2+1:
-                break
-
-    for r in range(x+d2+1, N+1): # 4번 선거구
-        for c in range(y-d1+d2-1, N+1):
-            p[3] += A[r-1][c-1]
-            if r-c == x-y-1-d1:
-                break
-
+    for r in range(1, N+1):
+        for c in range(1, N+1):
+            if 1 <= r < x+d1 and 1 <= c <= y:
+                if r+c <= x+y-1:
+                    s[r-1][c-1] = 1
+                    p[0] += A[r-1][c-1]
+            elif 1 <= r <= x+d2 and y < c <= N:
+                if r-c <= x-y-1:
+                    s[r-1][c-1] = 2
+                    p[1] += A[r-1][c-1]
+            elif x+d1 <= r <= N and 1 <= c < y-d1+d2:
+                if r-c > x+d1-y+d1:
+                    s[r-1][c-1] = 3
+                    p[2] += A[r-1][c-1]
+            elif x+d2 < r <= N and y-d1+d2 <= c <= N:
+                if r+c > x+y+d1+1:
+                    s[r-1][c-1] = 4
+                    p[3] += A[r-1][c-1]
+    # print()
+    # for row in s:
+    #     print(*row)
+    # print()
    
     p[4] = total_population - sum(p) # 5번 선거구
-    print(p, max(p), min(p))
+    # print(p, max(p), min(p))
     return  max(p) - min(p) # 인구가 가장 많은 선거구와 가장 적은 선거구의 인구 차이의 최솟값
+
+# calculate(2, 4, 2, 2)
+# calculate(2, 5, 3, 2)
+# calculate(4, 3, 1, 1)
 
 for d1 in range(1, N): # 1 <= d1, d2 -> 1 <= d1 < N
     for d2 in range(1, N-d1): # 1 <= d2 <= N-d1
