8df897eebd1ab27665698c0589ab9b6b9c17733f
Kyoung-yeon99 <itme2019@duksung.ac.kr>
2024-03-11 23:56:45 +0900

    week6 modified

--- Jump_to_Algorithm2/week6/KyoungyeonJu/4256 트리.py
+++ Jump_to_Algorithm2/week6/KyoungyeonJu/4256 트리.py
@@ -6,12 +6,11 @@ def post_order(pre, ino):
     if len(pre) == 0:
         return
     elif len(pre) == 1:
-        print(pre[0], end=" ")
+        print(pre[0])
         return
 
-
-    # 중위 순회(root-left-right) 루트 위치 -> left 개수
-    idx = ino.index(pre[0])
+    # 중위 순회(left-root-right) 루트 위치 -> left 개수
+    idx = ino.index(pre[0])  # pre[0] = 전위 순회 첫번째 요소는 root
 
     # pre - idx(left 개수)를 가지고 left, right slicing
     left_pre = pre[1:idx + 1]  # 전위 순회 - left
@@ -33,5 +32,5 @@ for _ in range(tc):
     pre = list(map(int, input().split()))  # root left right
     ino = list(map(int, input().split()))  # left root right
 
-    post_order(pre, ino)  # left right root
+    post_order(pre, ino)
     print()
