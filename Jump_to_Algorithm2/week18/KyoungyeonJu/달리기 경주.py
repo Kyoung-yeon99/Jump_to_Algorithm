f64381e221a22b9e8b4d93dcbca89fdf35356a29
Kyoung-yeon99 <itme2019@duksung.ac.kr>
2024-06-12 00:18:05 +0900

    week18

--- Jump_to_Algorithm2/week18/KyoungyeonJu/달리기 경주.py
+++ Jump_to_Algorithm2/week18/KyoungyeonJu/달리기 경주.py
@@ -1,4 +1,14 @@
 def solution(players, callings):
+    # 선수는 키, 인덱스는 키인 딕셔너리
+    d = {player: idx for idx, player in enumerate(players)}
+
+    for c in callings:
+        idx = d[c]  # 추월한 선수의 현재 위치
+        d[c] -= 1  # 추월한 선수의 추월 후 위치
+        d[players[idx-1]] += 1  # 추월 당한 선수의 추월 후 위치
+        players[idx-1], players[idx] = players[idx], players[idx-1]  # 리스트에서 선수들의 위치 swqp
+
+    return players
 
 """
 5 ≤ players의 길이 ≤ 50,000
