8d2f54931812ab2997f088cda422afc11382bc3e
hyunjiji0802 <guswl4174@naver.com>
2024-11-26 11:27:06 +0900

    week7

--- Jump_to_Algorithm3/week7/HyeonjiLee/7568 덩치.py
+++ Jump_to_Algorithm3/week7/HyeonjiLee/7568 덩치.py
@@ -1,17 +1,15 @@
 import sys
 N=int(sys.stdin.readline().rstrip())
 arr=[]
-
+answer = []
 for _ in range(N):
     arr.append(tuple(map(int, sys.stdin.readline().rstrip().split())))
 
-def 덩치(arr):
-    for i in range(N):
-        count = 1
-        for j in range(N):
-            if arr[i][0]<arr[j][0] and arr[i][1]<arr[j][1]:
-                count+=1
-        print(count, end=' ')
-
-덩치(arr)
-
+for i in range(N):
+    count = 1
+    for j in range(N):
+        #몸무게 arr[i][0], 키 arr[i][1] 모두 크면 count + 1
+        if arr[i][0]<arr[j][0] and arr[i][1]<arr[j][1]:
+            count+=1
+    answer.append(count)
+print(*answer)
